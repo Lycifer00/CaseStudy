fbaaa19acb728b05761f07b4cf89e4f8b722f0e3
Lycifer00 <lamanhb97@gmail.com>
2019-01-21 14:46:40 +0700

    can view list

--- src/main/java/com/lycifer/Note/controllers/NoteController.java
+++ src/main/java/com/lycifer/Note/controllers/NoteController.java
@@ -30,61 +30,61 @@ public class NoteController {
         return modelAndView;
     }
 
-    @GetMapping("/create-note")
-    public ModelAndView showCreateForm(){
-        ModelAndView modelAndView = new ModelAndView("create");
-        modelAndView.addObject("note",new Note());
-        return modelAndView;
-    }
-
-    @PostMapping("/create-note")
-    public ModelAndView saveNote(@ModelAttribute("note") Note note){
-        noteService.save(note);
-
-        ModelAndView modelAndView = new ModelAndView("create");
-        modelAndView.addObject("note", new Note());
-        modelAndView.addObject("message", "New note created successfully!");
-        return modelAndView;
-    }
-
-    @GetMapping("/edit-note/{id}")
-    public ModelAndView showEditForm(@PathVariable Long id){
-        Optional<Note> note = noteService.findById(id);
-        if(note.isPresent()) {
-            ModelAndView modelAndView = new ModelAndView("/edit");
-            modelAndView.addObject("note", note);
-            return modelAndView;
-
-        }else {
-            return new ModelAndView("/error-404");
-        }
-    }
-
-    @PostMapping("/edit-note")
-    public ModelAndView updateNote(@ModelAttribute("note") Note note){
-        noteService.save(note);
-        ModelAndView modelAndView = new ModelAndView("/edit");
-        modelAndView.addObject("note", note);
-        modelAndView.addObject("message", "Note updated successfully!");
-        return modelAndView;
-    }
-
-    @GetMapping("/delete-note/{id}")
-    public ModelAndView showDeleteForm(@PathVariable Long id){
-        Optional<Note> note = noteService.findById(id);
-        if(note.isPresent()) {
-            ModelAndView modelAndView = new ModelAndView("/delete");
-            modelAndView.addObject("note", note);
-            return modelAndView;
-
-        }else {
-            return new ModelAndView("/error-404");
-        }
-    }
-
-    @PostMapping("/delete-note")
-    public String deleteCustomer(@ModelAttribute("note") Note note){
-        noteService.remove(note.getId());
-        return "redirect:/";
-    }
+//    @GetMapping("/create-note")
+//    public ModelAndView showCreateForm(){
+//        ModelAndView modelAndView = new ModelAndView("create");
+//        modelAndView.addObject("note",new Note());
+//        return modelAndView;
+//    }
+//
+//    @PostMapping("/create-note")
+//    public ModelAndView saveNote(@ModelAttribute("note") Note note){
+//        noteService.save(note);
+//
+//        ModelAndView modelAndView = new ModelAndView("create");
+//        modelAndView.addObject("note", new Note());
+//        modelAndView.addObject("message", "New note created successfully!");
+//        return modelAndView;
+//    }
+//
+//    @GetMapping("/edit-note/{id}")
+//    public ModelAndView showEditForm(@PathVariable Long id){
+//        Optional<Note> note = noteService.findById(id);
+//        if(note.isPresent()) {
+//            ModelAndView modelAndView = new ModelAndView("/edit");
+//            modelAndView.addObject("note", note);
+//            return modelAndView;
+//
+//        }else {
+//            return new ModelAndView("/error-404");
+//        }
+//    }
+//
+//    @PostMapping("/edit-note")
+//    public ModelAndView updateNote(@ModelAttribute("note") Note note){
+//        noteService.save(note);
+//        ModelAndView modelAndView = new ModelAndView("/edit");
+//        modelAndView.addObject("note", note);
+//        modelAndView.addObject("message", "Note updated successfully!");
+//        return modelAndView;
+//    }
+//
+//    @GetMapping("/delete-note/{id}")
+//    public ModelAndView showDeleteForm(@PathVariable Long id){
+//        Optional<Note> note = noteService.findById(id);
+//        if(note.isPresent()) {
+//            ModelAndView modelAndView = new ModelAndView("/delete");
+//            modelAndView.addObject("note", note);
+//            return modelAndView;
+//
+//        }else {
+//            return new ModelAndView("/error-404");
+//        }
+//    }
+//
+//    @PostMapping("/delete-note")
+//    public String deleteCustomer(@ModelAttribute("note") Note note){
+//        noteService.remove(note.getId());
+//        return "redirect:/";
+//    }
 }
